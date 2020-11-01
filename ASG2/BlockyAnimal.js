5bc3850a9dcb6b88cdef85ffbd435e9ee29b4a92
Andrew Gavgavian <andrewgee7@gmail.com>
2020-10-31 17:12:21 -0700

    anger mode, toes, eyebrows, and more!

--- ASG2/BlockyAnimal.js
+++ ASG2/BlockyAnimal.js
@@ -29,7 +29,7 @@ let u_ModelMatrix;    // A GLSL uniform shader variable that allows the size to 
 let u_GlobalRotateMatrix;
 
 let g_globalAngleX = 0;
-let g_globalAngleY = 0;
+let g_globalAngleY = 45;
 let g_globalAngleZ = 0;
 let g_headAngle = 0;
 let g_tailAngle = 0;
@@ -148,12 +148,12 @@ function setUpAllEvents() {
 
 function resetCam() {
     g_globalAngleX = 0;
-    g_globalAngleY = 0;
+    g_globalAngleY = 45;
     g_globalAngleZ = 0;
 
     document.getElementById('XangleSlide').value = 0;
 
-    document.getElementById('YangleSlide').value = 0;
+    document.getElementById('YangleSlide').value = 45;
 
     document.getElementById('ZangleSlide').value = 0;
 
@@ -211,35 +211,57 @@ function updateAnimationAngles() {
 
 function setupCubes() {
     var globalRotMat = new Matrix4();
-    var body = new Cube([190 / 255, 187 / 255, 187 / 255, 1]);
-    var tail = new Cube([199 / 255, 196 / 255, 197 / 255, 1]);
-    var mane = new Cube([173 / 255, 167 / 255, 164 / 255, 1]);
-    var head = new Cube([140 / 255, 137 / 255, 137 / 255, 1]);
-    var mouth = new Cube([121 / 255, 103 / 255, 91 / 255, 1]);
-    var snout = new Cube([146 / 255, 121 / 255, 102 / 255, 1]);
-    var nose = new Cube([13 / 255, 14 / 255, 16 / 255, 1]);
-    var jaw = new Cube([13 / 255, 14 / 255, 16 / 255, 1]);
-    var leftEar = new Cube([160 / 255, 158 / 255, 158 / 255, 1]);
-    var rightEar = new Cube([160 / 255, 158 / 255, 158 / 255, 1]);
-    var leftIris = new Cube([184 / 255, 184 / 255, 184 / 255, 1]);
-    var rightIris = new Cube([184 / 255, 184 / 255, 184 / 255, 1]);
-    var leftPupil = new Cube([13 / 255, 14 / 255, 16 / 255, 1]);
-    var rightPupil = new Cube([13 / 255, 14 / 255, 16 / 255, 1]);
-    var flLeg = new Cube([146 / 255, 144 / 255, 145 / 255, 1]);
-    var frLeg = new Cube([146 / 255, 144 / 255, 145 / 255, 1]);
-    var blLeg = new Cube([146 / 255, 144 / 255, 145 / 255, 1]);
-    var brLeg = new Cube([146 / 255, 144 / 255, 145 / 255, 1]);
-    var boneMid = new Cube([227 / 255, 227 / 255, 227 / 255, 1]);
-    var boneLT = new Cube([227 / 255, 227 / 255, 227 / 255, 1]);
-    var boneLB = new Cube([227 / 255, 227 / 255, 227 / 255, 1]);
-    var boneRT = new Cube([227 / 255, 227 / 255, 227 / 255, 1]);
-    var boneRB = new Cube([227 / 255, 227 / 255, 227 / 255, 1]);
+    var body = new Cube(rgbaList(190, 187, 187));
+    var tail = new Cube(rgbaList(199, 196, 197));
+    var mane = new Cube(rgbaList(173, 167, 164));
+    var head = new Cube(rgbaList(140, 137, 137));
+    var mouth = new Cube(rgbaList(121, 103, 91));
+    var snout = new Cube(rgbaList(146, 121, 102));
+    var nose = new Cube(rgbaList(13, 14, 16));
+    var jaw = new Cube(rgbaList(13, 14, 16));
+    var leftEar = new Cube(rgbaList(160, 158, 158));
+    var rightEar = new Cube(rgbaList(160, 158, 158));
+    var leftIris = new Cube(rgbaList(184, 184, 184));
+    var rightIris = new Cube(rgbaList(184, 184, 184));
+    var leftPupil = new Cube(rgbaList(13, 14, 16));
+    var rightPupil = new Cube(rgbaList(13, 14, 16));
+    var flLeg = new Cube(rgbaList(146, 144, 145));
+    var frLeg = new Cube(rgbaList(146, 144, 145));
+    var blLeg = new Cube(rgbaList(146, 144, 145));
+    var brLeg = new Cube(rgbaList(146, 144, 145));
+    var boneMid = new Cube(rgbaList(227, 227, 227));
+    var boneLT = new Cube(rgbaList(227, 227, 227));
+    var boneLB = new Cube(rgbaList(227, 227, 227));
+    var boneRT = new Cube(rgbaList(227, 227, 227));
+    var boneRB = new Cube(rgbaList(227, 227, 227));
+    var earFloofL = new Cube(rgbaList(13, 14, 16));
+    var earFloofR = new Cube(rgbaList(13, 14, 16));
+    var eyebrowL = new Cube(rgbaList(13, 14, 16));
+    var eyebrowR = new Cube(rgbaList(13, 14, 16));
+
+    var flToe1 = new Cube(rgbaList(86, 82, 78));
+    var flToe2 = new Cube(rgbaList(199, 196, 193));
+    var flToe3 = new Cube(rgbaList(86, 82, 78));
+    var frToe1 = new Cube(rgbaList(86, 82, 78));
+    var frToe2 = new Cube(rgbaList(199, 196, 193));
+    var frToe3 = new Cube(rgbaList(86, 82, 78));
+    var blToe1 = new Cube(rgbaList(86, 82, 78));
+    var blToe2 = new Cube(rgbaList(199, 196, 193));
+    var blToe3 = new Cube(rgbaList(86, 82, 78));
+    var brToe1 = new Cube(rgbaList(86, 82, 78));
+    var brToe2 = new Cube(rgbaList(199, 196, 193));
+    var brToe3 = new Cube(rgbaList(86, 82, 78));
+
+    var flFoot = new Cube(rgbaList(130, 125, 128));
+    var frFoot = new Cube(rgbaList(130, 125, 128));
+    var blFoot = new Cube(rgbaList(130, 125, 128));
+    var brFoot = new Cube(rgbaList(130, 125, 128));
 
     g_ShapesList.push(globalRotMat,
         body, tail, mane, head, mouth,
         snout, nose, jaw, leftEar, rightEar,
         leftIris, rightIris, leftPupil, rightPupil,
-        flLeg, frLeg, blLeg, brLeg, boneMid, boneLT, boneLB, boneRT, boneRB);
+        flLeg, frLeg, blLeg, brLeg, boneMid, boneLT, boneLB, boneRT, boneRB, earFloofL, earFloofR, eyebrowL, eyebrowR, flToe1, flToe2, flToe3, frToe1, frToe2, frToe3, blToe1, blToe2, blToe3, brToe1, brToe2, brToe3, flFoot, frFoot, blFoot, brFoot);
 }
 // Goes through the global lists and renders all points on the page
 function renderAllShapes() {
@@ -247,8 +269,8 @@ function renderAllShapes() {
     // Clear <canvas>
     let globalRotMat = g_ShapesList[0];
     globalRotMat.setIdentity();
-    globalRotMat.rotate(g_globalAngleX, 0, 1, 0);
-    globalRotMat.rotate(g_globalAngleY, 1, 0, 0);
+    globalRotMat.rotate(g_globalAngleX, 1, 0, 0);
+    globalRotMat.rotate(g_globalAngleY, 0, 1, 0);
     globalRotMat.rotate(g_globalAngleZ, 0, 0, 1);
     gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
@@ -278,6 +300,28 @@ function renderAllShapes() {
     let boneLB = g_ShapesList[i++];
     let boneRT = g_ShapesList[i++];
     let boneRB = g_ShapesList[i++];
+    let earFloofL = g_ShapesList[i++];
+    let earFloofR = g_ShapesList[i++];
+    let eyebrowL = g_ShapesList[i++];
+    let eyebrowR = g_ShapesList[i++];
+
+    let flToe1 = g_ShapesList[i++];
+    let flToe2 = g_ShapesList[i++];
+    let flToe3 = g_ShapesList[i++];
+    let frToe1 = g_ShapesList[i++];
+    let frToe2 = g_ShapesList[i++];
+    let frToe3 = g_ShapesList[i++];
+    let blToe1 = g_ShapesList[i++];
+    let blToe2 = g_ShapesList[i++];
+    let blToe3 = g_ShapesList[i++];
+    let brToe1 = g_ShapesList[i++];
+    let brToe2 = g_ShapesList[i++];
+    let brToe3 = g_ShapesList[i++];
+
+    let flFoot = g_ShapesList[i++];
+    let frFoot = g_ShapesList[i++];
+    let blFoot = g_ShapesList[i++];
+    let brFoot = g_ShapesList[i++];
 
 
     body.matrix.setIdentity();
@@ -285,7 +329,7 @@ function renderAllShapes() {
     body.render();
 
     tail.matrix.setIdentity();
-    tail.matrix.rotate(10, 1, 0, 0);
+    tail.matrix.rotate(-10, 1, 0, 0);
     tail.matrix.rotate(-g_tailAngle, 0, 1, 0);
     tail.matrix.scale(.125, .125, .5);
     tail.matrix.translate(0, 0, 1.25);
@@ -343,7 +387,19 @@ function renderAllShapes() {
     rightEar.matrix.translate(1, 1.125, 1);
     rightEar.render();
 
-    toggleAnger(leftPupil, rightPupil);
+    earFloofL.matrix.setIdentity();
+    earFloofL.matrix.set(leftEar.matrix);
+    earFloofL.matrix.scale(.33, .5, .33);
+    earFloofL.matrix.translate(0, 0, -1.5);
+    earFloofL.render();
+
+    earFloofR.matrix.setIdentity();
+    earFloofR.matrix.set(rightEar.matrix);
+    earFloofR.matrix.scale(.33, .5, .33);
+    earFloofR.matrix.translate(0, 0, -1.5);
+    earFloofR.render();
+
+    toggleAnger(leftPupil, leftIris, rightPupil, rightIris);
 
     leftIris.matrix.setIdentity();
     leftIris.matrix.set(head.matrix);
@@ -356,6 +412,18 @@ function renderAllShapes() {
     rightIris.matrix.scale(.33, .2, .33);
     rightIris.matrix.translate(1, 0.5, -1.5);
     rightIris.render();
+
+    eyebrowL.matrix.setIdentity();
+    eyebrowL.matrix.set(leftIris.matrix);
+    eyebrowL.matrix.translate(0, 1.02, -.5);
+    eyebrowL.matrix.rotate(-20, 0, 0, 1);
+    eyebrowL.matrix.scale(1, .8, .33);
+
+    eyebrowR.matrix.setIdentity();
+    eyebrowR.matrix.set(rightIris.matrix);
+    eyebrowR.matrix.translate(0, 1.02, -.5);
+    eyebrowR.matrix.rotate(20, 0, 0, 1);
+    eyebrowR.matrix.scale(1, .8, .33);
 
     leftPupil.matrix.setIdentity();
     leftPupil.matrix.set(leftIris.matrix);
@@ -376,12 +444,60 @@ function renderAllShapes() {
     flLeg.matrix.translate(-.8, -.9, -1.5);
     flLeg.render();
 
+    flFoot.matrix.setIdentity();
+    flFoot.matrix.set(flLeg.matrix);
+    flFoot.matrix.scale(1.01, .15, 1.4);
+    flFoot.matrix.translate(0, -3.25, -0.1);
+    flFoot.render();
+
+    flToe1.matrix.setIdentity();
+    flToe1.matrix.set(flFoot.matrix);
+    flToe1.matrix.scale(.33, 1, .33);
+    flToe1.matrix.translate(-1, -0.01, -1.9);
+    flToe1.render();
+
+    flToe2.matrix.setIdentity();
+    flToe2.matrix.set(flFoot.matrix);
+    flToe2.matrix.scale(.33, 1, .33);
+    flToe2.matrix.translate(0, -0.01, -1.7);
+    flToe2.render();
+
+    flToe3.matrix.setIdentity();
+    flToe3.matrix.set(flFoot.matrix);
+    flToe3.matrix.scale(.33, 1, .33);
+    flToe3.matrix.translate(1, -0.01, -1.9);
+    flToe3.render();
+
     frLeg.matrix.setIdentity();
     frLeg.matrix.set(body.matrix);
     frLeg.matrix.rotate(g_legAngle, 1, 0, 0);
     frLeg.matrix.scale(.33, 1, .15);
     frLeg.matrix.translate(.8, -.9, -1.5);
     frLeg.render();
+
+    frFoot.matrix.setIdentity();
+    frFoot.matrix.set(frLeg.matrix);
+    frFoot.matrix.scale(1.01, .15, 1.4);
+    frFoot.matrix.translate(0, -3.25, -0.1);
+    frFoot.render();
+
+    frToe1.matrix.setIdentity();
+    frToe1.matrix.set(frFoot.matrix);
+    frToe1.matrix.scale(.33, 1, .33);
+    frToe1.matrix.translate(-1, -0.01, -1.9);
+    frToe1.render();
+
+    frToe2.matrix.setIdentity();
+    frToe2.matrix.set(frFoot.matrix);
+    frToe2.matrix.scale(.33, 1, .33);
+    frToe2.matrix.translate(0, -0.01, -1.7);
+    frToe2.render();
+
+    frToe3.matrix.setIdentity();
+    frToe3.matrix.set(frFoot.matrix);
+    frToe3.matrix.scale(.33, 1, .33);
+    frToe3.matrix.translate(1, -0.01, -1.9);
+    frToe3.render();
 
     blLeg.matrix.setIdentity();
     blLeg.matrix.set(body.matrix);
@@ -390,12 +506,60 @@ function renderAllShapes() {
     blLeg.matrix.translate(-.8, -.9, 2.5);
     blLeg.render();
 
+    blFoot.matrix.setIdentity();
+    blFoot.matrix.set(blLeg.matrix);
+    blFoot.matrix.scale(1.01, .15, 1.4);
+    blFoot.matrix.translate(0, -3.25, -0.1);
+    blFoot.render();
+
+    blToe1.matrix.setIdentity();
+    blToe1.matrix.set(blFoot.matrix);
+    blToe1.matrix.scale(.33, 1, .33);
+    blToe1.matrix.translate(-1, -0.01, -1.9);
+    blToe1.render();
+
+    blToe2.matrix.setIdentity();
+    blToe2.matrix.set(blFoot.matrix);
+    blToe2.matrix.scale(.33, 1, .33);
+    blToe2.matrix.translate(0, -0.01, -1.7);
+    blToe2.render();
+
+    blToe3.matrix.setIdentity();
+    blToe3.matrix.set(blFoot.matrix);
+    blToe3.matrix.scale(.33, 1, .33);
+    blToe3.matrix.translate(1, -0.01, -1.9);
+    blToe3.render();
+
     brLeg.matrix.setIdentity();
     brLeg.matrix.set(body.matrix);
     brLeg.matrix.rotate(g_legAngle, 1, 0, 0);
     brLeg.matrix.scale(.33, 1, .15);
     brLeg.matrix.translate(.8, -.9, 2.5);
     brLeg.render();
+
+    brFoot.matrix.setIdentity();
+    brFoot.matrix.set(brLeg.matrix);
+    brFoot.matrix.scale(1.01, .15, 1.4);
+    brFoot.matrix.translate(0, -3.25, -0.1);
+    brFoot.render();
+
+    brToe1.matrix.setIdentity();
+    brToe1.matrix.set(brFoot.matrix);
+    brToe1.matrix.scale(.33, 1, .33);
+    brToe1.matrix.translate(-1, -0.01, -1.9);
+    brToe1.render();
+
+    brToe2.matrix.setIdentity();
+    brToe2.matrix.set(brFoot.matrix);
+    brToe2.matrix.scale(.33, 1, .33);
+    brToe2.matrix.translate(0, -0.01, -1.7);
+    brToe2.render();
+
+    brToe3.matrix.setIdentity();
+    brToe3.matrix.set(brFoot.matrix);
+    brToe3.matrix.scale(.33, 1, .33);
+    brToe3.matrix.translate(1, -0.01, -1.9);
+    brToe3.render();
 
     boneMid.matrix.setIdentity();
     boneMid.matrix.set(jaw.matrix);
@@ -437,7 +601,10 @@ function renderAllShapes() {
         boneRT.render();
         boneRB.render();
     }
-
+    else {
+        eyebrowL.render();
+        eyebrowR.render();
+    }
 
 
 
@@ -457,25 +624,45 @@ function tick() {
     }
 }
 
-function toggleAnger(leftPupil, rightPupil) {
+function toggleAnger(leftPupil, leftIris, rightPupil, rightIris) {
     if (isAngry == false) {
-        if (leftPupil.color != [13 / 255, 14 / 255, 16 / 255, 1]) {
-            leftPupil.color = [13 / 255, 14 / 255, 16 / 255, 1];
+        if (leftPupil.color != rgbaList(13, 14, 16)) {
+            leftPupil.color = rgbaList(13, 14, 16);
             leftPupil.colorArray = makeArray(leftPupil.color);
         }
-        if (rightPupil.color != [13 / 255, 14 / 255, 16 / 255, 1]) {
-            rightPupil.color = [13 / 255, 14 / 255, 16 / 255, 1];
+        if (leftIris.color != rgbaList(184, 184, 184)) {
+            leftIris.color = rgbaList(184, 184, 184);
+            leftIris.colorArray = makeArray(leftIris.color);
+        }
+        if (rightPupil.color != rgbaList(13, 14, 16)) {
+            rightPupil.color = rgbaList(13, 14, 16);
             rightPupil.colorArray = makeArray(rightPupil.color);
+        }
+        if (rightIris.color != rgbaList(184, 184, 184)) {
+            rightIris.color = rgbaList(184, 184, 184);
+            rightIris.colorArray = makeArray(rightIris.color);
         }
     } else {
-        if (leftPupil.color != [228 / 255, 46 / 255, 46 / 255, 1]) {
-            leftPupil.color = [228 / 255, 46 / 255, 46 / 255, 1];
+        if (leftPupil.color != rgbaList(228, 46, 46)) {
+            leftPupil.color = rgbaList(228, 46, 46);
             leftPupil.colorArray = makeArray(leftPupil.color);
         }
-        if (rightPupil.color != [228 / 255, 46 / 255, 46 / 255, 1]) {
-            rightPupil.color = [228 / 255, 46 / 255, 46 / 255, 1];
+        if (leftIris.color != rgbaList(150, 11, 11)) {
+            leftIris.color = rgbaList(150, 11, 11);
+            leftIris.colorArray = makeArray(leftIris.color);
+        }
+        if (rightPupil.color != rgbaList(228, 46, 46)) {
+            rightPupil.color = rgbaList(228, 46, 46);
             rightPupil.colorArray = makeArray(rightPupil.color);
+        }
+        if (rightIris.color != rgbaList(150, 11, 11)) {
+            rightIris.color = rgbaList(150, 11, 11);
+            rightIris.colorArray = makeArray(rightIris.color);
         }
     }
 
+}
+
+function rgbaList(r, g, b, a = 1) {
+    return [r / 255, g / 255, b / 255, a]
 }
